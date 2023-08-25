2d96acb4c01279f08806c5e6cbd306a818062c35
Alejandra Delgado <alejandra.d08@outlook.com>
2023-08-24 23:45:52 -0500

    Parte 4

--- src/app/components/dashboards/productos/crear-producto/crear-producto.component.ts
+++ src/app/components/dashboards/productos/crear-producto/crear-producto.component.ts
@@ -1,11 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Route, Router } from '@angular/router';
+import { CrudService } from 'src/app/services/crud.service';
 
 @Component({
   selector: 'app-crear-producto',
   templateUrl: './crear-producto.component.html',
   styleUrls: ['./crear-producto.component.css']
 })
-export class CrearProductoComponent {
-
+export class CrearProductoComponent implements OnInit{
+formp : FormGroup
+  constructor(private fb: FormBuilder, private router: Router, private crudService: CrudService    
+    ){
+    this.formp = this.fb.group({
+      id: ['', Validators.required],
+      descripcion: ['', Validators.required],
+      observacion: ['', Validators.required],
+      provedor: ['', Validators.required],
+    });
+  }
+ngOnInit(): void {
+  
+}
+agregarProduto(){
+ 
+  this.crudService.agregarProducto(this.formp.value).subscribe();
+  this.router.navigate(['/dashboards/productos'])
+}
 }
--- src/app/components/dashboards/productos/productos.component.ts
+++ src/app/components/dashboards/productos/productos.component.ts
@@ -1,10 +1,70 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-
+import { CrudService } from 'src/app/services/crud.service';
+import * as XLSX from 'xlsx';
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 @Component({
   selector: 'app-productos',
   templateUrl: './productos.component.html',
   styleUrls: ['./productos.component.css']
 })
-export class ProductosComponent  {
- 
+export class ProductosComponent implements OnInit {
+  Productos: any;
+  constructor( private crudService:CrudService){
+
+  }
+ngOnInit(): void {
+  this.crudService.obtenerProducto().subscribe(respuesta=>{
+    console.log(respuesta);
+    this.Productos= respuesta;
+  });
+}
+
+borrarProductos(id:any, iControl:any){
+  console.log(id);
+  console.log(iControl);
+  if (window.confirm("Desea Borrar El Registro?")){
+  this.crudService.borrarProducto(id).subscribe((respuesta)=>{
+    this.Productos.splice(iControl,1);
+  });
+}
+ }
+  createPDF2() {
+    const documentDefinition = {
+      content: [
+        { text: 'Tabla de Productos', style: 'header' },
+        '\n',
+        {
+          table: {
+            headerRows: 1,
+            widths: ['', '', '', ''],
+            body: [
+              ['ID', 'Descripción', 'Observación', 'Proveedor'],
+              ...this.Productos.map(producto => [producto.id, producto.descripcion, producto.observacion, producto.provedor])
+            ]
+          }
+        }
+      ],
+      styles: {
+        header: {
+          fontSize: 18,
+          bold: true
+        }
+      }
+    };
+
+    pdfMake.createPdf(documentDefinition).download('tabla_productos.pdf');
+  }
+
+  createExcel() {
+    const ws = XLSX.utils.json_to_sheet(this.Productos);
+    const wb = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(wb, ws, 'Productos');
+    XLSX.writeFile(wb, 'productos.xlsx');
+  }
+  refreshPage(): void {
+    // Recarga la página
+    window.location.reload();
+  }
 }
