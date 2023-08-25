a1efd975031f4d8284abe825d27fa96b96ea4c9a
mateo-npb <mateo_3797@hotmail.com>
2023-08-24 23:36:24 -0500

    Parte3

--- src/app/components/dashboards/maquillaje/crear-maquillaje/crear-maquillaje.component.ts
+++ src/app/components/dashboards/maquillaje/crear-maquillaje/crear-maquillaje.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Route, Router } from '@angular/router';
+import { Crud3Service } from 'src/app/services/crud3.service';
 
 
 @Component({
@@ -6,6 +9,23 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './crear-maquillaje.component.html',
   styleUrls: ['./crear-maquillaje.component.css']
 })
-export class CrearMaquillajeComponent  {
+export class CrearMaquillajeComponent implements OnInit {
 
+formp : FormGroup
+  constructor(private fb: FormBuilder, private router: Router, private crudService: Crud3Service    
+    ){
+    this.formp = this.fb.group({
+      id: ['', Validators.required],
+      nombre: ['', Validators.required],
+      descripcion: ['', Validators.required],
+      provedor: ['', Validators.required],
+    });
+  }
+ngOnInit(): void {
+  
+}
+agregarMaquillaje(){
+  this.crudService.agregarMaquillaje(this.formp.value).subscribe();
+  this.router.navigate(['/dashboards/maquillaje'])
+}
 }
--- src/app/components/dashboards/maquillaje/maquillaje.component.ts
+++ src/app/components/dashboards/maquillaje/maquillaje.component.ts
@@ -1,13 +1,75 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-
+import { Crud3Service } from 'src/app/services/crud3.service';
+import * as XLSX from 'xlsx';
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 @Component({
   selector: 'app-maquillaje',
   templateUrl: './maquillaje.component.html',
   styleUrls: ['./maquillaje.component.css']
 })
-export class MaquillajeComponent {
- 
+export class MaquillajeComponent implements OnInit{
+  Maquillaje: any;
+  constructor( private crud3Service:Crud3Service){
+
+  }
+ngOnInit(): void {
+  this.crud3Service.obtenerMaquillaje().subscribe(respuesta=>{
+    console.log(respuesta);
+    this.Maquillaje= respuesta;
+  });
 }
 
+borrarMaquillaje(id:any, iControl:any){
+  console.log(id);
+  console.log(iControl);
+  if (window.confirm("Desea Borrar El Registro?")){
+  this.crud3Service.borrarMaquillaje(id).subscribe((respuesta)=>{
+    this.Maquillaje.splice(iControl,1);
+  });
+}
+ }
+ editarMaquillaje(){
 
+ }
+
+ createPDF2() {
+  const documentDefinition = {
+    content: [
+      { text: 'Tabla de Maquillaje', style: 'header' },
+      '\n',
+      {
+        table: {
+          headerRows: 1,
+          widths: ['*', '*', '*', '*'],
+          body: [
+            ['ID', 'nombre', 'descripcion', 'proveedor'],
+            ...this.Maquillaje.map(maquillaje => [maquillaje.id, maquillaje.nombre, maquillaje.descripcion, maquillaje.provedor])
+          ]
+        }
+      }
+    ],
+    styles: {
+      header: {
+        fontSize: 18,
+        bold: true
+      }
+    }
+  };
+
+  pdfMake.createPdf(documentDefinition).download('tabla_maquillaje.pdf');
+}
+
+createExcel() {
+  const ws = XLSX.utils.json_to_sheet(this.Maquillaje);
+  const wb = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(wb, ws, 'Maquillaje');
+  XLSX.writeFile(wb, 'Maquillaje.xlsx');
+}
+refreshPage(): void {
+  // Recarga la página
+  window.location.reload();
+}
+}
  
