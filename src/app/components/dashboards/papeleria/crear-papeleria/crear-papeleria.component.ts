2c1c4d7d989be98b915975817fea95e76720c5c9
Frank Ascuntar <acfrank001@gmail.com>
2023-08-24 23:27:01 -0500

    Parte 2

--- src/app/components/dashboards/papeleria/crear-papeleria/crear-papeleria.component.ts
+++ src/app/components/dashboards/papeleria/crear-papeleria/crear-papeleria.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Route, Router } from '@angular/router';
+import { Crud4Service } from 'src/app/services/crud4.service';
 
 @Component({
   selector: 'app-crear-papeleria',
@@ -8,4 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CrearPapeleriaComponent {
 
+  formp : FormGroup
+    constructor(private fb: FormBuilder, private router: Router, private crud4Service: Crud4Service    
+      ){
+      this.formp = this.fb.group({
+        id: ['', Validators.required],
+        producto: ['', Validators.required],
+        categoria: ['', Validators.required],
+        provedor: ['', Validators.required],
+      });
+    }
+  ngOnInit(): void {
+    
+  }
+  agregarPapeleria(){
+    this.crud4Service.agregarPapeleria(this.formp.value).subscribe();
+    this.router.navigate(['/dashboards/papeleria'])
+  }
 }
--- src/app/components/dashboards/papeleria/papeleria.component.ts
+++ src/app/components/dashboards/papeleria/papeleria.component.ts
@@ -1,10 +1,75 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
+import { Crud4Service } from 'src/app/services/crud4.service';
+import * as XLSX from 'xlsx';
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts';
+pdfMake.vfs = pdfFonts.pdfMake.vfs;
 @Component({
   selector: 'app-papeleria',
   templateUrl: './papeleria.component.html',
   styleUrls: ['./papeleria.component.css']
 })
-export class PapeleriaComponent{
+export class PapeleriaComponent implements OnInit{
 
-  
+  Papeleria: any;
+  constructor( private crud4Service:Crud4Service){
+
+  }
+ngOnInit(): void {
+  this.crud4Service.obtenerPapeleria().subscribe(respuesta=>{
+    console.log(respuesta);
+    this.Papeleria= respuesta;
+  });
+}
+
+borrarPapeleria(id:any, iControl:any){
+  console.log(id);
+  console.log(iControl);
+  if (window.confirm("Desea Borrar El Registro?")){
+  this.crud4Service.borrarPapeleria(id).subscribe((respuesta)=>{
+    this.Papeleria.splice(iControl,1);
+  });
+}
+ }
+ editarPapeleria(){
+
+ }
+
+ createPDF2() {
+  const documentDefinition = {
+    content: [
+      { text: 'Tabla de papeleria', style: 'header' },
+      '\n',
+      {
+        table: {
+          headerRows: 1,
+          widths: ['', '', '', ''],
+          body: [
+            ['ID', 'producto', 'categoria', 'provedor'],
+            ...this.Papeleria.map(papeleria => [papeleria.id, papeleria.producto, papeleria.categoria, papeleria.provedor])
+          ]
+        }
+      }
+    ],
+    styles: {
+      header: {
+        fontSize: 18,
+        bold: true
+      }
+    }
+  };
+
+  pdfMake.createPdf(documentDefinition).download('tabla_papeleria.pdf');
+}
+
+createExcel() {
+  const ws = XLSX.utils.json_to_sheet(this.Papeleria);
+  const wb = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(wb, ws, 'Papeleria');
+  XLSX.writeFile(wb, 'Papeleria.xlsx');
+}
+refreshPage(): void {
+  // Recarga la página
+  window.location.reload();
+}
 }
